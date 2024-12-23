fd7756b515143617b88fb79077d7b963133d7b07
Niklas-does-programming <84233627+Niklas-does-programming@users.noreply.github.com>
2024-12-23 18:41:31 +0100

    UserEditingScreen funktioniert

--- FinanzTracker/app/UserEditingScreen.jsx
+++ FinanzTracker/app/UserEditingScreen.jsx
@@ -1,79 +1,85 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, TextInput, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
 import NavBar from "./NavBar";
 import styles from "./styles/styles";
 import {
   getLoggedInNutzer,
   getNutzerByName,
 } from "../assets/logic/UserFunctions";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 import { changePassword } from "../assets/logic/FBAuth";
 import { FIREBASE_AUTH } from "../FirebaseConfig";
 
 const UserEditingScreen = () => {
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [user, setUser] = useState(getLoggedInNutzer());
 
   const navigation = useRouter();
-
-  const handleConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const user = FIREBASE_AUTH.currentUser;
-    if(user){
-    if (password === confirmPassword && password !== '') {
-      changePassword(password);
-      alert('Password changed successfully');
-    } else {
-      alert('Passwords do not match or are empty');
+    if (user) {
+      if (password === confirmPassword && password !== "") {
+        changePassword(password);
+        alert("Password changed successfully");
+      } else {
+        alert("Passwords do not match or are empty");
+      }
     }
-  }
   };
 
   const handleLogout = () => {
     navigation.push("/LoginScreen");
     clearAsyncStorage();
-    alert('Logged out successfully');
+    alert("Logged out successfully");
   };
 
-  
   const clearAsyncStorage = async () => {
     try {
       await AsyncStorage.clear();
-      console.log('AsyncStorage cleared');
+      console.log("AsyncStorage cleared");
     } catch (error) {
-      console.error('Error clearing AsyncStorage:', error);
+      console.error("Error clearing AsyncStorage:", error);
     }
   };
 
   return (
-    <View>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.header}>User Overview</Text>
-        <Text> {getLoggedInNutzer()} </Text>
-        <View>
-          <Text>New Password:</Text>
-          <TextInput secureTextEntry value={password} onChangeText={handlePasswordChange} />
-        </View>
-        <View>
-          <Text>Confirm Password:</Text>
-          <TextInput secureTextEntry value={confirmPassword} onChangeText={handleConfirmPasswordChange} />
-        </View>
-        <TouchableOpacity style={styles.loginButton} onPress={handleSubmit}>
-          <Text style={styles.loginButtonText}>Password ändern</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.loginButton} onPress={handleLogout}>
-          <Text style={styles.loginButtonText}>Logout</Text>
-        </TouchableOpacity>
-      </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.header}>User Overview</Text>
+      <Text>Benutzername:</Text>
+      <Text> {user} </Text>
+      <View>
+        <Text>New Password:</Text>
+        <TextInput
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+      <View>
+        <Text>Confirm Password:</Text>
+        <TextInput
+          secureTextEntry
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+        />
+      </View>
+      <TouchableOpacity style={styles.loginButton} onPress={handleSubmit}>
+        <Text style={styles.loginButtonText}>Password ändern</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogout}>
+        <Text style={styles.loginButtonText}>Logout</Text>
+      </TouchableOpacity>
+
       <NavBar />
     </View>
   );
